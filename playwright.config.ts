61e3a5f28c389f2a710a4dec60888134115de680
Gerry <gerry.waterston@xdesign.com>
2024-06-07 15:37:57 +0100

    Updates config file with screenshot

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,17 +10,23 @@ require('dotenv').config();
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  // testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
+  retries: 2,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -28,7 +34,23 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    // headless: false,
+    // ignoreHTTPSErrors: true,
+    // viewport: { width: 1280, height: 720 },
+    // video: 'on-first-retry',
   },
+  // timeout: 30000, //https://playwright.dev/docs/test-timeouts
+  // expect: {
+  /**
+   * Maximum time expect() should wait for the condition to be met.
+   * For example in `await expect(locator).toHaveText();`
+   */
+  // timeout: 10000,
+  // },
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  // outputDir: 'test-results/',
 
   /* Configure projects for major browsers */
   projects: [
